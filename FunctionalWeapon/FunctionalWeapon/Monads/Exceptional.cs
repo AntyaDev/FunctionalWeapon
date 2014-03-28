9780bac66312893d3c7b34e94bcb2e657bc2cc92
AntyaDev <antyadev@gmail.com>
2014-03-28 10:32:39 +0200

    Added Exceptional monad.

---
+++ FunctionalWeapon/FunctionalWeapon/Monads/Exceptional.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FunctionalWeapon.Monads
+{
+    public sealed class Exceptional<T>
+    {
+        readonly T _value;
+
+        public Exceptional(T value)
+        {
+            _value = value;
+        }
+
+        public Exceptional(Exception exception)
+        {
+            Exception = exception;
+            HasException = true;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (HasException) throw Exception;
+
+                return _value;
+            }
+        }
+
+        public bool HasException { get; private set; }
+
+        public Exception Exception { get; private set; }
+
+        public Exceptional<A> Bind<A>(Func<T, A> func)
+        {
+            try
+            {
+                return HasException
+                    ? new Exceptional<A>(Exception)
+                    : new Exceptional<A>(func(_value));
+            }
+            catch (Exception ex)
+            {
+                return new Exceptional<A>(ex);
+            }
+        }
+    }
+
+    public static class ExceptionalExtensions
+    {
+        public static Exceptional<T> ToExceptional<T>(this T value)
+        {
+            return new Exceptional<T>(value);
+        }
+    }
+}
